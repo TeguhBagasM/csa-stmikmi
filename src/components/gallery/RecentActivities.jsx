f02940fdb6ae0fd8fcf96bdc51eec5c1abe64bd7
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-09-10 07:26:56 +0700

    fix bug slider

--- src/components/gallery/RecentActivities.jsx
+++ src/components/gallery/RecentActivities.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
 import slide_image_1 from "./../../assets/images/kegiatan/kegiatan_1.jpg";
 import slide_image_2 from "./../../assets/images/kegiatan/kegiatan_2.jpg";
 import slide_image_3 from "./../../assets/images/kegiatan/kegiatan_3.jpg";
@@ -7,7 +9,8 @@ import slide_image_5 from "./../../assets/images/kegiatan/kegiatan_5.jpg";
 import slide_image_6 from "./../../assets/images/kegiatan/kegiatan_6.jpg";
 import slide_image_7 from "./../../assets/images/kegiatan/kegiatan_7.jpg";
 import slide_image_8 from "./../../assets/images/kegiatan/kegiatan_8.jpg";
-import { RxDotFilled } from "react-icons/rx";
+import slide_image_9 from "./../../assets/images/kegiatan/kegiatan_9.jpg";
+import slide_image_10 from "./../../assets/images/kegiatan/kegiatan_10.jpg";
 
 function RecentActivities() {
   const slides = [
@@ -38,25 +41,44 @@ function RecentActivities() {
     },
     {
       url: slide_image_6,
-      title: "Kegiatan 5",
-      description: "Deskripsi Kegiatan 5",
+      title: "Kegiatan 6",
+      description: "Deskripsi Kegiatan 6",
     },
     {
       url: slide_image_7,
-      title: "Kegiatan 5",
-      description: "Deskripsi Kegiatan 5",
+      title: "Kegiatan 7",
+      description: "Deskripsi Kegiatan 7",
     },
     {
       url: slide_image_8,
-      title: "Kegiatan 5",
-      description: "Deskripsi Kegiatan 5",
+      title: "Kegiatan 8",
+      description: "Deskripsi Kegiatan 8",
+    },
+    {
+      url: slide_image_9,
+      title: "Kegiatan 9",
+      description: "Deskripsi Kegiatan 9",
+    },
+    {
+      url: slide_image_10,
+      title: "Kegiatan 10",
+      description: "Deskripsi Kegiatan 10",
     },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const slidesPerView = 4;
-  const totalSlides = slides.length;
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
 
   const goToSlide = (slideIndex) => {
     setCurrentIndex(slideIndex);
@@ -64,14 +86,28 @@ function RecentActivities() {
 
   return (
     <div className="max-w-[1200px] mx-auto py-16 px-4 relative">
-      <div className="relative w-full overflow-hidden">
-        <div className="relative w-full">
+      <div className="relative w-full h-[400px]">
+        <div className="absolute inset-0 flex items-center justify-between px-4">
+          <div
+            className="hidden lg:block text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
+            onClick={prevSlide}
+          >
+            <BsChevronCompactLeft size={30} />
+          </div>
+          <div
+            className="hidden lg:block text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
+            onClick={nextSlide}
+          >
+            <BsChevronCompactRight size={30} />
+          </div>
+        </div>
+        <div className="overflow-hidden w-full h-full">
           <div
             className="flex transition-transform duration-500"
-            style={{ transform: `translateX(-${currentIndex * (100 / slidesPerView)}%)` }}
+            style={{ transform: `translateX(-${currentIndex * 100}%)` }}
           >
             {slides.map((slide, index) => (
-              <div key={index} className="flex-none w-full lg:w-[25%] p-2">
+              <div key={index} className="flex-none w-full lg:w-[33.333%] p-2">
                 <div
                   style={{ backgroundImage: `url(${slide.url})` }}
                   className="relative w-full h-[300px] bg-center bg-cover rounded-lg"
@@ -85,21 +121,17 @@ function RecentActivities() {
             ))}
           </div>
         </div>
-      </div>
-
-      {/* Dot Navigation */}
-      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex">
-        {slides.map((_, slideIndex) => (
-          <div
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
-            className={`text-2xl cursor-pointer ${
-              currentIndex === slideIndex ? "text-blue-500" : "text-gray-400"
-            }`}
-          >
-            <RxDotFilled />
-          </div>
-        ))}
+        <div className="flex top-4 justify-center py-2">
+          {slides.map((_, slideIndex) => (
+            <div
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+              className="text-2xl cursor-pointer text-gray-400 dark:text-gray-500"
+            >
+              <RxDotFilled />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
