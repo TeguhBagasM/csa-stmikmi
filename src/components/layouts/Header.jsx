1d26b4c8130666f58552815d38ae7df6b01c2254
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-10-20 12:22:06 +0700

    style(header): update ui logo

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -69,7 +69,7 @@ const Header = () => {
               onClick={handleFollow}
               className="flex items-center bg-gray-100 dark:bg-gray-900 py-2 px-3 rounded-lg shadow-md hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors duration-200"
             >
-              <FaInstagram className="text-2xl" />
+              <InstagramGradientIcon />
             </button>
           </div>
         </div>
