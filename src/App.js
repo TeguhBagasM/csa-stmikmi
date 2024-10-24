367500b719dd82b35de92a7987d13fcae3d72584
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-10-24 20:16:44 +0700

    chore(app.js): update routes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/layouts/Header";
 import Footer from "./components/layouts/Footer";
 import Home from "./pages/Home";
-import TentangKami from "./pages/TentangKami";
 import { ToasterProvider } from "./providers/toast-provider";
-import DivisiPemrograman from "./components/divisi/Pemrograman";
-import DivisiGame from "./components/divisi/Game";
-import DivisiIot from "./components/divisi/Iot";
-import DivisiMultimedia from "./components/divisi/Multimedia";
+// import TentangKami from "./pages/TentangKami";
+// import DivisiPemrograman from "./components/divisi/Pemrograman";
+// import DivisiGame from "./components/divisi/Game";
+// import DivisiIot from "./components/divisi/Iot";
+// import DivisiMultimedia from "./components/divisi/Multimedia";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -28,11 +28,11 @@ const App = () => {
       <ToasterProvider />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/tentang-kami" element={<TentangKami />} />
+        {/* <Route path="/tentang-kami" element={<TentangKami />} />
         <Route path="/divisi-pemrograman" element={<DivisiPemrograman />} />
         <Route path="/divisi-game" element={<DivisiGame />} />
         <Route path="/divisi-iot" element={<DivisiIot />} />
-        <Route path="/divisi-multimedia" element={<DivisiMultimedia />} />
+        <Route path="/divisi-multimedia" element={<DivisiMultimedia />} /> */}
       </Routes>
       <Footer />
     </div>
